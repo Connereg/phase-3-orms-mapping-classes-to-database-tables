ec95d87864c4e748c58f551c96487ae96b0802c5
Conner Gleason <connereg@optonline.net>
2021-09-25 16:13:09 -0400

    Completed Lab + Additional Code

--- lib/song.rb
+++ lib/song.rb
@@ -2,31 +2,14 @@ require "pry"
 
 class Song
 
-  attr_accessor :name, :album, :id
+  attr_reader :id
+  attr_accessor :name, :album
 
-  def initialize(name:, album:, id: nil)
+
+  def initialize(id= nil, name, album)
     @id = id
     @name = name
     @album = album
-  end
-
-  def save
-    sql = <<-SQL
-      INSERT INTO songs (name, album)
-      VALUES (?, ?)
-    SQL
-
-    DB[:conn].execute(sql,self.name,self.album)
-
-    self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
-
-    self
-  end
-
-
-  def self.create(name:, album:)
-    song = Song.new(name: name, album: album)
-    song.save
   end
 
   def self.create_table
@@ -39,12 +22,46 @@ class Song
       SQL
    DB[:conn].execute(sql)
   end
+
+  def save
+    if self.id
+      self.update
+    else
+    sql = <<-SQL
+      INSERT INTO songs (name, album)
+      VALUES (?, ?)
+    SQL
+
+    DB[:conn].execute(sql,self.name, self.album)
+
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+    end
+  end
+
+
+  def self.create(name:, album:)
+    song = Song.new(name: name, album: album)
+    song.save
+    song
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM songs WHERE name = ?"
+    result = DB[:conn].execute(sql, name)[0]
+    Song.new(result[0], result[1], result[2])
+  end
+
+  def update
+    sql = "UPDATE songs SET name = ?, album = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.album, self.id)
+  end
+
 #
 end
 
 
 
-gold_digger = Song.new(name: "Gold Digger", album: "Late Registration")
 
-wonk = Song.new(name: "Wonk", album: "Honeymoon")
+
+
 
